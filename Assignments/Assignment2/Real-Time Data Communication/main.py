a5f1440a0ad0dc935aaf0071028cf196d601d72d
Joshua <jdthomas@mavs.coloradomesa.edu>
2025-01-29 20:46:57 -0700

    Changes

---
+++ Assignments/Assignment2/Real-Time Data Communication/main.py
@@ -0,0 +1,21 @@
+import time
+import serial
+
+arduinoData = serial.Serial('/dev/cu.usbmodem14101', 9600)
+time.sleep(1)
+
+while True:
+    while arduinoData.inWaiting() == 0:
+        pass
+    
+    dataPacket = arduinoData.readline()
+    dataPacket = str(dataPacket, 'utf-8')
+    dataPacket = dataPacket.strip('\r\n')
+    splitPacket = dataPacket.split(",")
+
+    #print(dataPacket)
+    x = float(splitPacket[0])
+    y = float(splitPacket[1])
+    z = float(splitPacket[2])
+
+    print("X=", x, "Y=", y, "Z=", z)
