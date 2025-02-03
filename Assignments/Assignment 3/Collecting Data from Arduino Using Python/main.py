8b75fe99246ca3c8c7d00abfba5dec8df41b9e16
Joshua <jdthomas@mavs.coloradomesa.edu>
2025-02-03 13:39:42 -0700

    Changes

---
+++ Assignments/Assignment 3/Collecting Data from Arduino Using Python/main.py
@@ -0,0 +1,24 @@
+import time
+import serial
+
+# Replace with your actual serial port (check in Arduino IDE)
+arduinoData = serial.Serial('/dev/cu.usbmodem14301', 9600)
+
+time.sleep(2)  # Allow time for the connection to establish
+
+while True:
+    # print(type(arduinoData.in_waiting))
+    if arduinoData.in_waiting == 0: 
+        break
+        
+    dataPacket = arduinoData.readline()
+    # print(f"debug: {dataPacket}")
+
+    dataPacket = dataPacket.decode('utf-8').strip()  # Read and clean the data
+    potValue = int(dataPacket)  # Convert to integer
+    
+    print("Potentiometer Value:", potValue)
+
+    time.sleep(0.5)
+
+arduinoData.close()  # Ensure the serial connection is properly closed
